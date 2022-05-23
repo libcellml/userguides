250fe8a57182c64a57f5776909eb279d2b18e092
Hugh Sorby <h.sorby@auckland.ac.nz>
2022-05-23 22:26:48 +1200

    Ignore README.rst from sphinx analysis.

--- conf.py
+++ conf.py
@@ -130,7 +130,9 @@ release = 'latest'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = []
+exclude_patterns = [
+    'README.rst',
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
